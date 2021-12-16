707ff35398048a85bebc26d55bc28c22e873d34e
Tiago Lopes <saxtiago14@gmail.com>
2021-12-16 10:32:21 -0300

    Classe votos feita. Falta relacionamento

---
+++ src/main/java/dev/model/entities/SituacaoAtualTopico.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev.model.entities;
+
+import lombok.Getter;
+
+/**
+ *
+ * @author tiago
+ */
+@Getter
+public enum SituacaoAtualTopico {
+    
+    UPVOTE("Curti"), 
+    DOWNVOTE("Descurtir");
+
+    private String descricao;
+
+    private SituacaoAtualTopico(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
---
+++ src/main/java/dev/model/entities/Voto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev.model.entities;
+
+/**
+ *
+ * @author tiago
+ */
+public class Voto {
+    
+    private int id;
+    private Integer upVote;
+    private Integer downVote;
+    
+    
+}
